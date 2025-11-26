751509b2aa2349f91c4906799ff714994a41e993
Dakotah Jennifer <dakjencreativellc@gmail.com>
2025-11-25 20:36:14 -0500

    fix: increase product button size

--- src/app/dashboard/products/page.tsx
+++ src/app/dashboard/products/page.tsx
@@ -11,23 +11,23 @@ export default function ProductsPage() {
           <h2 className="text-2xl font-bold text-gray-800 mb-4">internal: business branding</h2>
           <div className="overflow-x-auto">
             <div className="grid grid-flow-col auto-cols-max gap-4"> {/* Products horizontally in a grid */}
-              <button className="aspect-square border border-transparent rounded-md shadow-sm text-lg font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 flex flex-col items-center justify-center text-center p-4">
+              <button className="w-40 h-40 border border-transparent rounded-md shadow-sm text-lg font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 flex flex-col items-center justify-center text-center p-4">
                 <AppWindow size={48} />
                 <span className="mt-2">Website</span>
               </button>
-              <button className="aspect-square border border-transparent rounded-md shadow-sm text-lg font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 flex flex-col items-center justify-center text-center p-4">
+              <button className="w-40 h-40 border border-transparent rounded-md shadow-sm text-lg font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 flex flex-col items-center justify-center text-center p-4">
                 <AtSign size={48} />
                 <span className="mt-2">Email</span>
               </button>
-              <button className="aspect-square border border-transparent rounded-md shadow-sm text-lg font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 flex flex-col items-center justify-center text-center p-4">
+              <button className="w-40 h-40 border border-transparent rounded-md shadow-sm text-lg font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 flex flex-col items-center justify-center text-center p-4">
                 <Brush size={48} />
                 <span className="mt-2">Portfolio</span>
               </button>
-              <button className="aspect-square border border-transparent rounded-md shadow-sm text-lg font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 flex flex-col items-center justify-center text-center p-4">
+              <button className="w-40 h-40 border border-transparent rounded-md shadow-sm text-lg font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 flex flex-col items-center justify-center text-center p-4">
                 <Laptop size={48} />
                 <span className="mt-2">Product 1.4</span>
               </button>
-              <button className="aspect-square border border-transparent rounded-md shadow-sm text-lg font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 flex flex-col items-center justify-center text-center p-4">
+              <button className="w-40 h-40 border border-transparent rounded-md shadow-sm text-lg font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 flex flex-col items-center justify-center text-center p-4">
                 <Book size={48} />
                 <span className="mt-2">Product 1.5</span>
               </button>
@@ -41,7 +41,7 @@ export default function ProductsPage() {
           <div className="overflow-x-auto">
             <div className="grid grid-flow-col auto-cols-max gap-4"> {/* Products horizontally in a grid */}
               <Link href="/dashboard/courses/create">
-                <button className="aspect-square border border-transparent rounded-md shadow-sm text-lg font-medium text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 flex flex-col items-center justify-center text-center p-4">
+                <button className="w-40 h-40 border border-transparent rounded-md shadow-sm text-lg font-medium text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 flex flex-col items-center justify-center text-center p-4">
                   <div className="relative">
                     <LaptopMinimal size={48} />
                     <BadgeCheck size={24} className="absolute -top-2 -right-2 text-white bg-green-600 rounded-full" />
@@ -49,15 +49,15 @@ export default function ProductsPage() {
                   <span className="mt-2">Course</span>
                 </button>
               </Link>
-              <button className="aspect-square border border-transparent rounded-md shadow-sm text-lg font-medium text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 flex flex-col items-center justify-center text-center p-4">
+              <button className="w-40 h-40 border border-transparent rounded-md shadow-sm text-lg font-medium text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 flex flex-col items-center justify-center text-center p-4">
                 <BookMarked size={48} />
                 <span className="mt-2">Library</span>
               </button>
-              <button className="aspect-square border border-transparent rounded-md shadow-sm text-lg font-medium text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 flex flex-col items-center justify-center text-center p-4">
+              <button className="w-40 h-40 border border-transparent rounded-md shadow-sm text-lg font-medium text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 flex flex-col items-center justify-center text-center p-4">
                 <CalendarCheck2 size={48} />
                 <span className="mt-2">Book a call</span>
               </button>
-              <button className="aspect-square border border-transparent rounded-md shadow-sm text-lg font-medium text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 flex flex-col items-center justify-center text-center p-4">
+              <button className="w-40 h-40 border border-transparent rounded-md shadow-sm text-lg font-medium text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 flex flex-col items-center justify-center text-center p-4">
                 <BadgePlus size={48} />
                 <span className="mt-2">Custom</span>
               </button>
@@ -70,19 +70,19 @@ export default function ProductsPage() {
           <h2 className="text-2xl font-bold text-gray-800 mb-4">Products</h2>
           <div className="overflow-x-auto">
             <div className="grid grid-flow-col auto-cols-max gap-4"> {/* Products horizontally in a grid */}
-              <button className="aspect-square border border-transparent rounded-md shadow-sm text-lg font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 flex flex-col items-center justify-center text-center p-4">
+              <button className="w-40 h-40 border border-transparent rounded-md shadow-sm text-lg font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 flex flex-col items-center justify-center text-center p-4">
                 <CalendarClock size={48} />
                 <span className="mt-2">Book Me</span>
               </button>
-              <button className="aspect-square border border-transparent rounded-md shadow-sm text-lg font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 flex flex-col items-center justify-center text-center p-4">
+              <button className="w-40 h-40 border border-transparent rounded-md shadow-sm text-lg font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 flex flex-col items-center justify-center text-center p-4">
                 <Combine size={48} />
                 <span className="mt-2">Deliverables</span>
               </button>
-              <button className="aspect-square border border-transparent rounded-md shadow-sm text-lg font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 flex flex-col items-center justify-center text-center p-4">
+              <button className="w-40 h-40 border border-transparent rounded-md shadow-sm text-lg font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 flex flex-col items-center justify-center text-center p-4">
                 <CopyPlus size={48} />
                 <span className="mt-2">Campaign</span>
               </button>
-              <button className="aspect-square border border-transparent rounded-md shadow-sm text-lg font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 flex flex-col items-center justify-center text-center p-4">
+              <button className="w-40 h-40 border border-transparent rounded-md shadow-sm text-lg font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 flex flex-col items-center justify-center text-center p-4">
                 <Laptop size={48} />
                 <span className="mt-2">Product 3.4</span>
               </button>
